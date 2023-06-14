1d79f893a03286b59633df30561182fd9a60ff72
ArchLeaders <archparliament@gmail.com>
2023-06-13 17:10:07 -0700

    Create service extension

---
+++ src/TotkPlugin.cs
@@ -0,0 +1,13 @@
+﻿using NxEditor.Plugin;
+
+namespace NxEditor.TotkPlugin;
+
+public class TotkPlugin : IServiceExtension
+{
+    public string Name { get; } = "TotK Plugin";
+
+    public void RegisterExtension(IServiceManager serviceManager)
+    {
+        throw new NotImplementedException();
+    }
+}
