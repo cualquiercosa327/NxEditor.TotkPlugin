767e1429162456245996c15c391468eaa685d96b
ArchLeaders <archparliament@gmail.com>
2023-06-14 01:20:48 -0700

    Create TotkConfig extension

---
+++ src/TotkConfig.cs
@@ -0,0 +1,11 @@
+﻿using NxEditor.Plugin.Extensions;
+
+namespace NxEditor.TotkPlugin;
+
+internal class TotkConfig : IConfigExtension
+{
+    public static TotkConfig Shared { get; } = new();
+
+    public string Name { get; set; } = "TotK Config";
+    public string GamePath { get; set; } = string.Empty;
+}
